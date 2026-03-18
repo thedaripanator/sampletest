b57b13e5a96fde4d55613cba8e5fe1fbd90d4112
thedaripanator <sayondeep1618@gmail.com>
2026-03-19 00:36:44 +0530

    Test Deployment

---
+++ sampletest/src/main/java/com/example/sampletest/Controller/Home.java
@@ -0,0 +1,13 @@
+package com.example.sampletest.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+}
---
+++ sampletest/src/main/java/com/example/sampletest/SampletestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampletestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampletestApplication.class, args);
+	}
+
+}
